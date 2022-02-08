0693e843b45459a57e41ba402313f7b6b0c2941d
Victor Rodrigues <rodrigueseti@outlook.com>
2022-02-07 21:31:59 -0300

    sprite.js renamed to pigSprite.js

--- objectsVariables/pigSprite.js
+++ objectsVariables/pigSprite.js
@@ -3,8 +3,8 @@ let pigSprite;
 let xPigPos = 100;
 let yPigPos = 365;
 
-let xPigSpeed = 3;
-let yPigSpeed = 4;
+let xPigSpeed = 10;
+let yPigSpeed = 10;
 
 const pigWidth = 30;
 const pigHeight = 30;
